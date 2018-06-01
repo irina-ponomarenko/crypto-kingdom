433fd4a3973a7780a9ded81ca837f2d80c3a806b
Admin <ponomarenko02041993@gmail.com>
2018-06-01 23:40:24 +0300

    statistics games media

--- js/js.js
+++ js/js.js
@@ -1,5 +1,42 @@
 $(document).ready(function () {
-    $(".icon-block").on('click', function() {
-        $(".left-content").toggle();
+	$('.click-menu').click(function() {
+		$('.left-content').hide(200);
+		$('.wrapper-hide').css('display', 'block');
+	})
+	$('.click-hide').click(function() {
+		$('.left-content').show(200);
+		$('.wrapper-hide').css('display', 'none');
+	});
+	$('.withdraw').click( function(event) {
+        $('.success')
+            .css('display', 'flex') // убирaем у мoдaльнoгo oкнa display: none;
+            .animate({opacity: 1,  right: '20px'}, 200);
+        $('.success-click').click(function(){
+            $('.success')
+                .animate({opacity: 1, right: '-430px'}, 200);
+        });
+	});
+    $('#save').click( function(event) {
+        $('.success')
+            .css('display', 'flex') // убирaем у мoдaльнoгo oкнa display: none;
+            .animate({opacity: 1,  right: '20px'}, 200);
+        $('.success-click').click(function(){
+            $('.success')
+                .animate({opacity: 1, right: '-430px'}, 200);
+        });
     });
-})
+    $('#error-save').click( function(event) {
+        $('.error')
+            .css('display', 'flex') // убирaем у мoдaльнoгo oкнa display: none;
+            .animate({opacity: 1,  right: '20px'}, 200);
+        $('.error-click').click(function(){
+            $('.error')
+                .animate({opacity: 1, right: '-430px'}, 200);
+        });
+    });
+    $('.btn-js').on('click', function() {
+        var copyText = document.getElementById("myInput");
+        copyText.select();
+        document.execCommand("copy");
+    })
+});
