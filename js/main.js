5c755e72b157cf321aee57b0ec47b4c8937121bb
Admin <ponomarenko02041993@gmail.com>
2018-06-11 23:08:27 +0300

    new

--- js/main.js
+++ js/main.js
@@ -103,4 +103,22 @@ $(document).ready(function () {
         $(this).css('display', 'none');
         $('.accept').css('display', 'block');
     });
+
+
+    var time = 60;
+    var initialOffset = '380';
+    var i = 1
+
+    /* Need initial run as interval hasn't yet occured... */
+    $('.circle_animation').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
+
+    var interval = setInterval(function() {
+        $('h5').text(i);
+        if (i == time) {
+            clearInterval(interval);
+            return;
+        }
+        $('.circle_animation').css('stroke-dashoffset', initialOffset-((i+1)*(initialOffset/time)));
+        i++;
+    }, 1000);
 });
